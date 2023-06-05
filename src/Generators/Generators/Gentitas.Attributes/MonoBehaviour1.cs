76cc3e8b794c2a9ca74854e04aacca3f8aced387
rapushka <mikalajsiatkouski@gmail.com>
2023-06-05 16:58:23 +0300

    projects separated in two

---
+++ src/Generators/Generators/Gentitas.Attributes/MonoBehaviour1.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace Gentitas.Attributes
+{
+	public class MonoBehaviour1 : MonoBehaviour { }
+}
---
+++ src/Generators/Generators/Gentitas.Plugins/MonoBehaviour1.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+namespace Gentitas.Plugins
+{
+	public class MonoBehaviour1 : MonoBehaviour { }
+}
